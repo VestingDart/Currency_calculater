3eb8b0cfeef4caa6d05c1e31f3842e39df787d93
VestingDart <sasuke.uchiha2010@yahoo.com>
2025-03-19 13:33:43 +0100

    Euro to dollar currency addet.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer money = 10;
+        convertToDollars(money);
+    }
+
+    public static void convertToDollars(Integer eur) {
+        System.out.println("The amount in dollars is: " + eur * 1.09);
     }
 }
